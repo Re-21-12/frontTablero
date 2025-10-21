5d45dd80eaca217e128e953b22d7533a8e84679a
Re-21-12 <victora.caas@gmail.com>
2025-10-20 21:40:18 -0600

    [ADD] added app.corazondeseda.lat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -33,7 +33,7 @@ export const environment: Environment = {
       clientId: 'frontend',
     },
     homepage: 'https://app.corazondeseda.lat',
-    redirect: 'https://auth.corazondeseda.lat',
+    redirect: 'https://app.corazondeseda.lat',
   },
   dev: {
     apiBaseUrl: 'http://localhost:5000/api',

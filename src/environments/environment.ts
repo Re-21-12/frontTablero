62ce0b0774738f09972a47f4e73532e51632316d
Re-21-12 <victora.caas@gmail.com>
2025-11-07 19:24:16 -0600

    [ADDED] merge

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -35,7 +35,7 @@ export const environment: Environment = {
     },
     homepage: 'https://backoffice.corazondeseda.lat',
     redirect: 'https://backoffice.corazondeseda.lat',
-    socketUrl: 'http://socket-service:3000',
+    socketUrl: 'https://socket.corazondeseda.lat',
   },
   dev: {
     apiBaseUrl: 'http://localhost:5000/api',
